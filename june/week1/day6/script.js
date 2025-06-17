74a5a3ef6df394788d390690911d747de96c73c4
sankar110000 <skaibalya748@gmail.com>
2025-06-17 11:02:19 +0530

    upto date

---
+++ june/week1/day6/script.js
@@ -0,0 +1,68 @@
+// let a = 20;
+// console.log(a);
+
+// let A = 30;
+// console.log(A);
+
+// let name = "Sankar";
+// console.log(name);
+
+// let _user = "user1"
+
+// let $name = "Kaibalya"
+
+// // varibale declartion
+
+// // using var 
+// // global level
+// var b = 30;
+// console.log(b);
+
+// function declare(){
+//     var clg = "GIET"; 
+//     let role = "Student"
+//     console.log(b);
+// }
+
+// declare()
+// // function level
+// // console.log(role);
+
+// // let globally
+// let product  = "phone"
+// console.log(product);
+
+
+
+// // let fucntion level
+function printProductInfo(){
+    let productId = 1;
+}
+
+const arr = [1, 2, 4, 5];
+
+console.log(typeof(arr));
+
+
+
+// // console.log(productId);
+
+// let movie = "Avatar"
+// movie = "Avengers" 
+// console.log(movie);
+
+// // decalration and initialization
+// var salary;
+// let salary;
+// salary = 20000
+// console.log(salary);
+
+
+let schoolName;
+schoolName = "HRM";
+
+const bike = "ZX10R";
+
+
+const sy = Symbol("?")
+console.log(sy);
