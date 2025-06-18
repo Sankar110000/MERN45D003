8800bc15440d3b7953c2e7e1b222f829b2ed7aba
sankar110000 <skaibalya748@gmail.com>
2025-06-18 10:55:30 +0530

    Added loops

---
+++ june/week2/day2/script.js
@@ -0,0 +1,65 @@
+// // Arithmatic Operator
+// console.log(11+2);
+
+// // unary operator
+// let a = 10;
+// console.log(a++);
+// console.log(++a); 
+
+// console.log(a--);
+// console.log(--a);
+
+// // Assignment Operator
+// let name = "Sankar"
+// console.log(typeof (name+2));
+
+
+// let str1 = "text"
+// let str2 = "text"
+// let num1 = 1;
+// let num2 = "1"
+
+// console.log(num1 === num2);
+
+// // logical operators
+
+// console.log((2>3) || (4>3));
+// console.log(!!NaN);
+
+// // condtional statement
+// let mode = "light"
+// let color = "blue"
+
+// if (mode == "dark-mode") {
+//     color = "black"
+// }else{
+//     color = "light"
+// }
+
+// console.log(color);
+
+// let age = 20;
+// console.log(age > 18 ? "Adult" : "Not an adult");
+
+
+// const num = prompt("Enter a number : ");
+// let num = 15;
+// console.log(num%5 == 0 ? "Multiple of 5" : "Not an multiiple of 5");
+
+
+// loops
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+    
+// }
+
+let i = 1;
+// while(i){
+//     console.log(i);
+//     i++
+// }
+
+do {
+    console.log(i);
+    i++
+} while (i <= 10);
